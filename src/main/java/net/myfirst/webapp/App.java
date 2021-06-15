5e5d16c8dfbed31bfc7ff07a6ccac61dc77e2153
LuyoloLuja <luyolom19@gmail.com>
2021-06-15 21:32:51 +0200

    added greeted users route

--- src/main/java/net/myfirst/webapp/App.java
+++ src/main/java/net/myfirst/webapp/App.java
@@ -3,10 +3,8 @@ package net.myfirst.webapp;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 
 import static spark.Spark.*;
 
@@ -54,16 +52,39 @@ public class App {
         post("/hello", (req, res) -> {
 
             Map<String, Object> map = new HashMap<>();
-            String greeting = "Hello, " + req.queryParams("username");
-            map.put("greeting", greeting);
+
+            String username = req.queryParams("username");
+            String language = req.queryParams("language");
+            String greetMessage = "";
+
+            if (!username.equals("") && !language.equals("")) {
+                if (language.equals("english")) {
+                    greetMessage = "Hello, " + username;
+                } else if(language.equals("isixhosa")) {
+                    greetMessage = "Molo, " + username;
+                } else if (language.equals("afrikaans")) {
+                    greetMessage = "More, " + username;
+                }
+            }
+            map.put("greeting", greetMessage);
 
             return new ModelAndView(map, "hello.handlebars");
 
         }, new HandlebarsTemplateEngine());
 
         get("/greeted", (req, res) -> {
-//            return new ModelAndView(map, "hello.handlebars");
-            return null;
+
+            Map<String, String> map = new HashMap<>();
+            String username = req.queryParams("username");
+
+            map.put("greetedNames", username);
+
+            for (Map.Entry<String, String> entrySet : map.entrySet()){
+                System.out.println(entrySet);
+                map.put("greetedNames", entrySet.getValue());
+            }
+
+            return new ModelAndView(map, "greeted.handlebars");
         });
     }
 }
